103d0fe072b75f51270b1c8c79d862320c9fc9ae
Taylor Rosby <taylorrosby@gmail.com>
2019-02-25 17:31:41 -0700

    Add project files.

---
+++ Operation/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Operation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Operation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(OperationModel model, string command)
+        {
+            if(command == "add")
+            {
+                model.Result = model.A + model.B;
+            }
+            if(command == "sub")
+            {
+                model.Result = model.A - model.B;
+            }
+            if(command == "mul")
+            {
+                model.Result = model.A * model.B;
+            }
+            if(command == "div")
+            {
+                model.Result = model.A / model.B;
+            }
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Operation/Models/OperationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Operation.Models
+{
+	public class OperationModel
+	{
+		public double A { get; set; }
+		public double B { get; set; }
+		public double Result { get; set; }
+	}
+}
